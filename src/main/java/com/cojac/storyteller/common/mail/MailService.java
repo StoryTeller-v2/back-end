2c4687c95be4a23030c7a0fe42369df439ec76f4
pyounani <yndbsk9372@gmail.com>
2025-01-05 18:53:46 +0900

    [Feat] 비동기 방식으로 변경 #4

--- src/main/java/com/cojac/storyteller/common/mail/MailService.java
+++ src/main/java/com/cojac/storyteller/common/mail/MailService.java
@@ -8,6 +8,7 @@ import org.springframework.mail.MailSendException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.retry.annotation.Retryable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,7 +19,8 @@ public class MailService {
 
     private final JavaMailSender emailSender;
 
-//    @Retryable(interceptor = "mailRetryInterceptor")
+    @Retryable(interceptor = "mailRetryInterceptor")
+//    @Async("mailServiceTaskExecutor")
     public void sendEmail(String toEmail,
                           String title,
                           String text) {
